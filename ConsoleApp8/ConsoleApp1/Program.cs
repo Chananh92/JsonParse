d9f018253d2cec90e3d2a2c97536750614a10c60
Chananh92 <Chananh6992@gmail.com>
2018-03-20 14:04:30 +0200

    wasdsaadsd

---
+++ ConsoleApp8/ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    interface ISerializableProcessor
+    {
+
+    }
+
+    class Program
+    {
+        class Product
+        {
+            public string Name { get; set; }
+            public DateTime ExpiryDate { get; set; }
+            public decimal Price { get; set; }
+            public IList<string> Sizes { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+
+            product.Name = "Apple";
+            product.ExpiryDate = new DateTime(2008, 12, 28);
+            product.Price = 3.99M;
+            product.Sizes = new string[] { "Small", "Medium", "Large" };
+
+            string output = JsonConvert.SerializeObject(product);
+
+            Console.WriteLine(output);
+
+            Product deserializedProduct = JsonConvert.DeserializeObject<Product>(output);
+
+        }
+
+        static string SerializeObject(object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        static T DeserializeObject<T>(string jsonObj)
+        {
+            return JsonConvert.DeserializeObject<T>(jsonObj);
+        }
+    }
+}
---
+++ ConsoleApp8/ConsoleApp2/Program.cs
@@ -0,0 +1,110 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonText = System.IO.File.ReadAllText(@"json.txt");
+
+            IDictionary<string, object> RequestElements = new Dictionary<string, object>();
+
+            RequestElements = GetDictionary(jsonText);
+
+            foreach (var item in RequestElements)
+            {
+                Console.WriteLine(item.Key + " " + item.Value.ToString());
+                Console.WriteLine(item.Value.GetType());
+                Console.WriteLine();
+            }
+        }
+
+
+        static IDictionary<string, object> GetDictionary(string json)
+        {
+            if (!(json.StartsWith("{") && json.EndsWith("}")))
+                json = "{" + json + "}";
+
+            var jsonObject = JObject.Parse(json);
+
+            return GetDictionary(jsonObject);
+        }
+
+        static IDictionary<string, object> GetDictionary(JToken json)
+        {
+            var jsonObject = json.Value<JObject>();
+
+            return GetDictionary(jsonObject);
+        }
+
+        static IDictionary<string, object> GetDictionary(JObject json)
+        {
+            var innerRequestElements = new Dictionary<string, object>();
+
+            foreach (var item in json)
+            {
+                var value = GetObject(item.Value);
+
+                if (value != null)
+                {
+                    innerRequestElements.Add(item.Key, value);
+                }
+            }
+
+            return innerRequestElements;
+        }
+
+        static object GetObject(JToken jToken)
+        {
+            object obj = null;
+
+            switch (jToken.Type)
+            {
+                case JTokenType.Integer:
+                    obj = Convert.ToInt32(jToken.ToString());
+                    break;
+                case JTokenType.Float:
+                    obj = Convert.ToDecimal(jToken.ToString());
+                    break;
+                case JTokenType.String:
+                    obj = jToken.ToString();
+                    break;
+                case JTokenType.Boolean:
+                    obj = Convert.ToBoolean(jToken.ToString());
+                    break;
+                case JTokenType.Date:
+                    obj = Convert.ToDateTime(jToken.ToString());
+                    break;
+                case JTokenType.Array:
+                    obj = GetArray(jToken);
+                    break;
+                case JTokenType.Object:
+                    obj = GetDictionary(jToken);
+                    break;
+            }
+
+            return obj;
+        }
+
+        static IList<object> GetArray(JToken jToken)
+        {
+            var valueList = new List<object>();
+
+            var jArray = JArray.Parse(jToken.ToString());
+
+            foreach (var item in jArray)
+            {
+                var value = GetObject(item);
+                valueList.Add(value);
+            }
+
+            return valueList;
+        }
+    }
+}
